57a8bd04fa4f437fd2ca841dc742e141da33c942
shan-debug <shanley.rosero@netfluencecorp.com>
2024-04-15 05:24:14 -0400

    fix:  fixed response.js to support existing thread conversation
    added storing of `threadId` in local storage

--- api/src/functions/response.js
+++ api/src/functions/response.js
@@ -2,6 +2,8 @@ const { app } = require('@azure/functions');
 
 const  { 
     createThreadAndRun, 
+    addMessageToThread,
+    createRun,
     listMessages, 
     getRun, 
     submitToolOutputsToRun 
@@ -48,9 +50,10 @@ const getResolvedToolOutput = (toolCall) => {
 };
 
 const generateResponse = async (q) => {
+    const assistant_id = 'asst_fRiGSuHQxjFh3Ooy5BtAoqgb';
 
     // Create Thread and Run
-    let runResponse = await createThreadAndRun(q);
+    let runResponse = await createThreadAndRun(assistant_id, q);
 
     // Get Thread ID and Run ID
     const threadId = runResponse.thread_id;
@@ -103,14 +106,77 @@ const generateResponse = async (q) => {
     return runMessages.data;
 }
 
-app.http('response', {
+
+const existingThreadResponse = async (threadId, q) => {
+    const assistant_id = 'asst_fRiGSuHQxjFh3Ooy5BtAoqgb';
+
+    // Create Thread and Run
+    let threadResponse = await addMessageToThread(threadId, q);
+    let runResponse = await createRun(assistant_id, threadId);
+
+    // Get Thread ID and Run ID
+    const runId = runResponse.id;
+
+    // Poll Run Status
+    do {
+        await new Promise((resolve) => setTimeout(resolve, 800));
+
+        // Get Run Status
+        runResponse = await getRun(threadId, runId);
+        console.log("Run Status: ", runResponse.status);
+
+
+        // Check if run requires action
+        if (runResponse.status === "requires_action" && runResponse.required_action?.type === "submit_tool_outputs") {
+            console.log("Submitting tool outputs...");
+            const toolOutputs = [];
+            // Get Resolved Tool Output
+            if (runResponse.required_action?.submit_tool_outputs?.tool_calls) {
+                for (const toolCall of runResponse.required_action?.submit_tool_outputs.tool_calls) {
+                    toolOutputs.push(getResolvedToolOutput(toolCall));
+                }
+                runResponse = await submitToolOutputsToRun(
+                    threadId,
+                    runResponse.id,
+                    toolOutputs,
+                );
+            }
+        }
+        // Check if run is queued or in progress
+    } while (runResponse.status === "queued" || runResponse.status === "in_progress")
+
+    // List Messages
+    const runMessages = await listMessages(threadId);
+
+    // Log Messages
+    for (let i = runMessages.data.length - 1; i >= 0; i--) {
+        const runMessageDatum = runMessages.data[i]
+        for (const item of runMessageDatum.content) {
+            if (item.type === "text") {
+                console.log(`${runMessageDatum.role}: ${item.text?.value}`);
+            } else if (item.type === "image_file") {
+                console.log(`Assistant message file id: ${item.imageFile?.fileId}`);
+            }
+        }
+    }
+
+    // Return Messages
+    return runMessages.data;
+}
+
+
+app.http('messages', {
     methods: ['POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
         
         const { query } = JSON.parse(await request.text());
-        context.log(`Query: ${query}`);
+        const threadId = request.query.get('threadId') || '';
+
+        context.log(`threadId: ${threadId}`);
+        context.log(`query: ${query}`);
+
 
         // Check body for query
         if (!query) {
@@ -124,14 +190,29 @@ app.http('response', {
         }
 
         try {
-            const response = await generateResponse(query);
-            return { 
-                status: 200,
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(response, null, 2)
-            };
+            if (threadId) {
+                const response = await existingThreadResponse(threadId, query);
+                return { 
+                    status: 200,
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        messages: response
+                    }, null, 2)
+                };
+            } else {
+                const response = await generateResponse(query);
+                return { 
+                    status: 200,
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        messages: response
+                    }, null, 2)
+                };
+            }
         } catch (error) {
             context.log(`Error: ${error}`);
             return { 
